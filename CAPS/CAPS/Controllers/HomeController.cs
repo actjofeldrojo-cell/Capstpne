03dcd8f9e3fa19979bf405bba0016eeb5deea3a1
jeffjude-c <jeffjudelasaca@gmail.com>
2025-08-14 22:04:11 +0800

    This is a sample commit
    * Fetch origin
    * Commit the files you want
    * Push origin
    * Go to history and compare to main
    * If no changes (behind) go to Branch > Create Pull Request
    * If there is changes, go to Branch > Update Main, then push origin, then Branch > Create Pull Request
    * Redirect to the website, click 'Confirm Merge', then 'Create Pull Request'.
    * Done, go back to github desktop, then fetch origin.
    * The ahead now becomes behind from main. Go to Branch > Update from main.
    * Push origin.
    * Done.

--- CAPS/CAPS/Controllers/HomeController.cs
+++ CAPS/CAPS/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace CAPS.Controllers
             return View();
         }
 
+        public ActionResult kupal()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
